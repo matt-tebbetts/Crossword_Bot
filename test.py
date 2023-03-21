119503cce0294f2dba2887e9737ea836090f0178
Matt Tebbetts <matttebbetts@gmail.com>
2023-03-20 22:59:14 -0400

    No more csv
    Only writing to SQL, also gets users from table rather than users.csv

--- test.py
+++ test.py
@@ -1,20 +1,44 @@
+import os
+import re
+import requests
 import pandas as pd
-import bot_functions as b
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from datetime import datetime, timedelta
+from io import StringIO
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-# save loc
-my_fil = 'files/images/test.png'
+# Load MySQL details from environment variables
+load_dotenv()
+sql_pass = os.getenv("SQLPASS")
+sql_user = os.getenv("SQLUSER")
+sql_host = os.getenv("SQLHOST")
+sql_port = os.getenv("SQLPORT")
+database = os.getenv("SQLDATA")
+sql_addr = f"mysql+pymysql://{sql_user}:{sql_pass}@{sql_host}:{sql_port}/{database}"
+engine = create_engine(sql_addr)
 
-# read test csv to dataframe
-df = pd.read_csv('files/mini_history.csv')
-df = df[df['game_date']=='2023-03-15']
-df = df[df['added_ts']=='2023-03-14 22:29:10']
+url = "http://nyt.aromatt.net/times/"
+response = requests.get(url)
+html_content = response.text
 
-# render table as figure and save
-fig = b.render_mpl_table(df, chart_title='test').figure
-fig.savefig(my_fil, dpi=300, bbox_inches='tight', pad_inches=0.25)
+csv_files = re.findall(r"(\d{4}-\d{2}-\d{2}\.csv)", html_content)
 
-image = mpimg.imread(my_fil)
-plt.imshow(image)
-plt.show()
+dfs = []
+
+for csv_file in csv_files:
+    game_date = datetime.strptime(csv_file[:-4], "%Y-%m-%d")
+    if (datetime.now() - game_date).days <= 90:
+        csv_url = url + csv_file
+        csv_content = requests.get(csv_url).text
+        df = pd.read_csv(StringIO(csv_content))
+        df.insert(0, "game_date", game_date)
+        dfs.append(df)
+
+combined_df = pd.concat(dfs)
+combined_df.columns = ['game_date', 'player_id', 'game_time']
+
+# Add timestamp
+combined_df['py_insert_ts'] = datetime.now()
+
+# Send the combined DataFrame to the MySQL database
+combined_df.to_sql('mini_backups', engine, if_exists='append', index=False)
