a3d71e30ca2d75957f3b78e8db68bebd8894372f
Matt Tebbetts <matttebbetts@gmail.com>
2023-04-12 22:26:04 -0400

    auto_fetch time improvements

--- test.py
+++ test.py
@@ -1,21 +1,22 @@
-import bot_functions
-import os
-from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-game_name = 'mini'
-time_frame = 'this year'
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1920,1200")
 
+# Change the path to the Chrome webdriver on your system
+driver_path = "C:/Users/matt_/chromedriver.exe"
+service = Service(executable_path=driver_path)
+driver = webdriver.Chrome(service=service, options=options)
 
-# get the min_date and max_date based on the user's input
-date_range = bot_functions.get_date_range(time_frame)
-min_date, max_date = date_range
+url = "https://public.tableau.com/app/profile/matthew.tebbetts/viz/Mini_16795384393570/Leaderboard?publish=yes"
+driver.get(url)
 
-# get image
-img_path = bot_functions.get_leaderboard(
-                                        guild_id="672233217985871908",
-                                        game_name=game_name,
-                                        min_date=min_date,
-                                        max_date=max_date)
+# Give some time for the Tableau Public page to load
+driver.implicitly_wait(60)
 
-
-Image.open(img_path).show()
+screenshot_path = "files/images/tableau_screenshot.png"
+driver.save_screenshot(screenshot_path)
+driver.quit()
