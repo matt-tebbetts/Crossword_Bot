893680115683e542365509ec49c5be1beb333b98
Matt Tebbetts <matttebbetts@gmail.com>
2023-04-15 19:33:50 -0400

    fixed mini date

---
+++ playlist_creator.py
@@ -0,0 +1,104 @@
+import os
+import re
+import json
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+YOUTUBE_CLIENT_SECRET = json.loads(os.getenv('YOUTUBE_CLIENT_SECRET'))
+YOUTUBE_SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+YOUTUBE_URL_REGEX = r'(https?://)?(www\.)?(youtube\.com|youtu\.?be)/\S+'
+
+
+def youtube_auth():
+    if os.path.exists(CREDENTIALS_FILE):
+        # Load the stored credentials from the file
+        credentials = Credentials.from_authorized_user_file(CREDENTIALS_FILE)
+    else:
+        # Prompt the user for authorization
+        flow = InstalledAppFlow.from_client_info(json.loads(YOUTUBE_CLIENT_SECRET), SCOPES)
+        credentials = flow.run_local_server(port=0)
+        
+        # Save the credentials to the file for future use
+        with open(CREDENTIALS_FILE, 'w') as credentials_file:
+            credentials_file.write(credentials.to_json())
+        print(f"Credentials saved to {CREDENTIALS_FILE}")
+
+    return credentials
+
+
+
+def create_youtube_playlist(youtube, title, description):
+    request = youtube.playlists().insert(
+        part="snippet,status",
+        body={
+            "snippet": {
+                "title": title,
+                "description": description,
+            },
+            "status": {
+                "privacyStatus": "private"
+            }
+        }
+    )
+    response = request.execute()
+    return response['id']
+
+
+def add_video_to_playlist(youtube, playlist_id, video_id):
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": playlist_id,
+                "resourceId": {
+                    "kind": "youtube#video",
+                    "videoId": video_id
+                }
+            }
+        }
+    )
+    request.execute()
+
+
+def extract_video_ids(youtube_links):
+    video_ids = []
+    for link in youtube_links:
+        video_id = None
+        if 'youtu.be' in link:
+            video_id = link.split('/')[-1]
+        elif 'youtube.com' in link:
+            video_id = re.search(r'v=([^&]+)', link).group(1)
+        
+        if video_id:
+            video_ids.append(video_id)
+    return video_ids
+
+
+async def create_playlist_from_discord_history(channel, playlist_title, playlist_description):
+    youtube_links = []
+    async for message in channel.history(limit=1000):
+        links = re.findall(YOUTUBE_URL_REGEX, message.content)
+        if links:
+            youtube_links.extend(links)
+
+    if not youtube_links:
+        print("No YouTube links found in the Discord chat history.")
+        return
+
+    youtube = build('youtube', 'v3', credentials=youtube_auth())
+    playlist_id = create_youtube_playlist(youtube, playlist_title, playlist_description)
+
+    video_ids = extract_video_ids(youtube_links)
+    for video_id in video_ids:
+        try:
+            print(f"Attempting to add video with ID {video_id} to the playlist.")  # Added print statement
+            add_video_to_playlist(youtube, playlist_id, video_id)
+            print(f"Added video with ID {video_id} to the playlist.")
+        except HttpError as error:
+            print(f"An error occurred while adding video with ID {video_id} to the playlist: {error}")
+
+    print(f"Playlist created with ID {playlist_id}.")
--- test.py
+++ test.py
@@ -1,69 +1,39 @@
 import os
-import re
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
-import pytz
-from io import StringIO
-from config import credentials, sql_addr
-from sqlalchemy import create_engine
+import discord
+from discord import Intents
+from dotenv import load_dotenv
+from playlist_creator import create_playlist_from_discord_history
+import asyncio
 
-# The URL to be scraped
-url = "http://nyt.aromatt.net/times/"
+load_dotenv()
 
-# Fetch the HTML content using requests
-response = requests.get(url)
+DISCORD_TOKEN = os.getenv('CROSSWORD_BOT')
+THREAD_ID = 1072246720580296736
 
-# Initialize an empty DataFrame to store the data
-combined_df = pd.DataFrame()
+intents = Intents.all()
+intents.typing = False
+intents.presences = False
 
-if response.status_code == 200:
-    # Parse the HTML content using Beautiful Soup
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    # Find all 'a' tags with an href attribute containing '.csv'
-    csv_links = soup.find_all('a', href=re.compile(r'\.csv'))
+client = discord.Client(intents=intents)
 
-    # Iterate through the CSV links
-    for link in csv_links:
-        # Extract the CSV URL and file name
-        csv_url = os.path.join(url, link['href'])
-        file_name = link['href']
+async def main():
+    await client.start(DISCORD_TOKEN)
 
-        # Extract the date from the file name
-        date_match = re.search(r'(\d{4}-\d{2}-\d{2})', file_name)
-        game_date = date_match.group(1) if date_match else None
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-        # Download the CSV and read it into a DataFrame
-        csv_response = requests.get(csv_url)
-        csv_content = csv_response.content.decode('utf-8')
+    # Get the thread using the hardcoded ID
+    thread = await client.fetch_channel(THREAD_ID)
 
-        # Create a DataFrame from the CSV content
-        df = pd.read_csv(StringIO(csv_content))
+    if thread:
+        # Call the create_playlist_from_discord_history function
+        await create_playlist_from_discord_history(thread, "Test Playlist 2", "Test playlist created from Discord chat history")
+        print("Playlist creation process completed.")
+    else:
+        print(f"Thread with ID '{THREAD_ID}' not found.")
 
-        # Add a 'game_date' column
-        df['game_date'] = game_date
+    # Close the Discord connection
+    await client.close()
 
-        # Convert the current time to Eastern Time and add an 'added_ts' column
-        eastern = pytz.timezone('US/Eastern')
-        added_ts = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
-
-        # Rename and reorder columns
-        df = df.rename(columns={'name': 'player_id', 'time': 'game_time'})
-        df['player_id'] = df['player_id'].str.lower()
-        df = df[['game_date', 'player_id', 'game_time']]
-
-        # Append the DataFrame to the combined DataFrame
-        combined_df = pd.concat([combined_df, df], ignore_index=True)
-
-    # add timestamp
-    combined_df['added_ts'] = added_ts
-    combined_df.to_csv('files/test.csv', index=False)
-
-    engine = create_engine(sql_addr)
-    combined_df.to_sql('mini_history', engine, if_exists='append', index=False)
-
-    print("ok, done")
-else:
-    print(f"Failed to fetch the URL. Status code: {response.status_code}")
+asyncio.run(main())
