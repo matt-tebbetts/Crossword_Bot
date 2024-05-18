a90fb4fd347d67e61b2534aecc882165d621529d
Matt Tebbetts <matttebbetts@gmail.com>
2024-05-18 18:04:00 -0400

    added chromedriver options

--- global_functions.py
+++ global_functions.py
@@ -139,6 +139,8 @@ def get_webdriver():
     options = Options()
     options.binary_location = get_path('chrome')
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     return webdriver.Chrome(service=service, options=options)
 
