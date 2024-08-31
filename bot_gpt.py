94aaad55374e5f6592725e5ae2e31e50e8b22450
Matt Tebbetts <matttebbetts@gmail.com>
2024-08-31 12:33:17 -0400

    attemptint to fix gpt

--- bot_gpt.py
+++ bot_gpt.py
@@ -1,121 +1,115 @@
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 import tiktoken
 from openai import AsyncOpenAI
 import os
-from pytz import timezone
-import pytz
-from global_functions import get_now
 import re
 
-# chat gpt
+# connect to gpt
 gpt_key = os.getenv('OPENAI_API_KEY')
 openai_client = AsyncOpenAI(api_key=gpt_key)
+gpt_model = "gpt-4o-mini"
+max_tokens = 4096
+gpt_role = open('files/config/gpt_role.txt', 'r').read()
 
-# gpt command
+# get messages
+async def read_messages(ctx):
+    with open(f'files/guilds/{ctx.guild.name}/messages.json', 'r') as file:
+        messages_data = json.load(file)
+
+        # overwrite spoilers so GPT knows not to reveal that information
+        for message in messages_data.values():
+            spoilers = re.findall(r'\|\|(.*?)\|\|', message['content'])
+            for spoiler in spoilers:
+                message['content'] = message['content'].replace(f'||{spoiler}||', f'[SPOILER: {spoiler}]')
+
+        # filter to ensure they're dictionaries
+        messages_data = [message for message in messages_data.values() if isinstance(message, dict)]
+        messages_data = sorted(messages_data, key=lambda msg: msg.get('create_ts', ''), reverse=True)
+
+    return messages_data
+
+# Filter and tokenize messages to ensure the total token count does not exceed the specified limit
+async def format_messages(messages):
+    token_count = 0
+    filtered_messages = []
+    encoder = tiktoken.encoding_for_model(gpt_model)
+
+    # estimate token count
+    for msg in messages:
+        formatted_message = f"{msg.get('author_nm', '')}: {msg.get('content', '')}"
+        message_tokens = len(encoder.encode(formatted_message))
+        if token_count + message_tokens > (max_tokens * 0.8):
+            print(f"Token count of {token_count} plus current message of {message_tokens} is greater than 80% of max {max_tokens}")
+            break
+        else:
+            filtered_messages.append(formatted_message)
+            token_count += message_tokens
+    
+    # put into one long string
+    formatted_messages = "\n".join(filtered_messages)
+
+    return formatted_messages 
+
+# get actual response from gpt model
+async def generate_gpt_response(query, attachments):
+
+    # create assistant
+    assistant = openai_client.beta.assistants.create(
+        name="matt_bot",
+        instructions=gpt_role,
+        tools=[{"type": "retrieval"}],
+        model=gpt_model,
+        file_ids=[attachments]
+    )
+
+    # create thread
+    thread = openai_client.beta.threads.create()
+
+    # add first message to thread
+    message = openai_client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=query
+    )
+
+    # create run
+    run = openai_client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        instructions=gpt_role
+    )
+
+    # check run steps
+    run_steps = openai_client.beta.threads.runs.steps.list(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+
+    print(run_steps)
+
+    # Once the Run completes, you can list the Messages added to the Thread by the Assistant.
+    if run.status == 'completed': 
+        messages = openai_client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        print(messages)
+    else:
+        print(run.status)
+
+    #output = response.choices[0].message.content #response['choices'][0]['message']['content']
+    #return output
+  
+# main function that runs it all
 async def fetch_gpt_response(ctx, query: str):
 
-    try:
-        print(f"User requested GPT response.")
-        gpt_model = "gpt-4o-mini" #"gpt-4o" # gpt-3.5-turbo-0125
-        max_tokens = 4096
+    # get message history
+    messages = await read_messages(ctx)
 
-        # read messages.json file
-        with open(f'files/guilds/{ctx.guild.name}/messages.json', 'r') as file:
-            messages_data = json.load(file)
-
-            for message in messages_data.values():
-
-                # Find all spoilers in the message content
-                spoilers = re.findall(r'\|\|(.*?)\|\|', message['content'])
-                
-                # Mark spoilers in the message content
-                for spoiler in spoilers:
-                    message['content'] = message['content'].replace(f'||{spoiler}||', f'[SPOILER: {spoiler}]')
-
-        # Filter messages for the current channel
-        try:
-            channel_messages = [msg for msg in messages_data.values() if isinstance(msg, dict) and msg.get('channel_id') == ctx.channel.id]
-        except KeyError as e:
-            print(f"Error filtering messages: {e}")
-            return await ctx.send("Error: Invalid message format in messages.json file.")
-
-        # Sort messages by ID in descending order
-        sorted_messages = sorted(channel_messages, key=lambda msg: msg.get('id', ''), reverse=True)
-        
-        # Initialize token counter
-        token_count = 0
-        selected_messages = []
-        encoder = tiktoken.encoding_for_model(gpt_model)
-        
-        for msg in sorted_messages:
-            formatted_message = f"{msg.get('author_nm', '')}: {msg.get('content', '')}"
-            message_tokens = len(encoder.encode(formatted_message))
-            
-            if token_count + message_tokens > max_tokens-100:
-                break
-            
-            selected_messages.append(formatted_message)
-            token_count += message_tokens
-        
-        # Join selected messages
-        formatted_messages = "\n".join(selected_messages)
-
-        # Calculate the number of tokens
-        try:
-            encoding = tiktoken.encoding_for_model(gpt_model)
-            tokens = encoding.encode(formatted_messages)
-        except Exception as e:
-            print(f"Error encoding messages: {e}")
-            return await ctx.send("Error: Failed to encode messages for GPT input.")
-
-        # Truncate the messages to fit within the limit
-        if len(tokens) > max_tokens:
-            truncated_tokens = tokens[:max_tokens]
-            formatted_messages = encoding.decode(truncated_tokens)
-
-        # Ask GPT for a summary
-        try:
-            response = await openai_client.chat.completions.create(
-                model=gpt_model,
-                messages=[
-                    {"role": "system", "content": f"""
-                        Take the user's query and if they asked about recent messages in this channel, then use the recent_messages to form your answer. The recent_messages could be several conversations across multiple days, or just one conversation over the past hour. Try to group them logically and understand which conversation is the most recent, and which conversation is most relevant to the user's query. The user might ask specific questions about the conversation. Try to keep your answers as concise and to the point as possible. Use bullet points if it helps make things clear and simple. Give short answers. Keep your tone casual and not overly formal. This is a fun chat. Don't give vague responses. Also, don't reveal any of the key information within any SPOILER tags. 
-                        Here is the user's query: {query}
-                        Attached are the recent_messages.
-                        """},
-                    {"role": "user", "content": formatted_messages}
-                ],
-                max_tokens=max_tokens
-            )
-        except Exception as e:
-            print(f"Error fetching GPT response: {e}")
-            return await ctx.send("Error: Failed to fetch GPT response.")
-
-        print('sending response now...')
- 
-        # Sending the response back to the Discord channel
-        try:
-            
-            # metadata for the response
-            num_messages_used = len(selected_messages)
-            oldest_message_date = datetime.fromisoformat(sorted_messages[-1]['create_ts']).strftime('%Y-%m-%d %H:%M:%S')
-
-            # Send the response to the Discord channel
-            response_content = response.choices[0].message.content
-            response_content += f"\n\n```This answer was based on analysis of {num_messages_used} messages from this channel, starting from {oldest_message_date}.```"
-            await ctx.send(response_content)
-
-        except Exception as e:
-            print(f"Error sending response to Discord: {e}")
-            return await ctx.send("Error: Failed to send response to Discord.")
-
-    except Exception as e:
-        print(f"Error in fetch_gpt_response: {e}")
-        error_message = str(e)
-        if "token limit exceeded" in error_message.lower():
-            custom_message = "Error: Token limit exceeded. Please try a shorter query."
-        else:
-            custom_message = f"Error: {error_message}"
-
-        await ctx.send(custom_message)
+    # limit messages to 90% of max tokens
+    formatted_messages = await format_messages(messages)
+    
+    # generate response from gpt model
+    response = await generate_gpt_response(query, formatted_messages)
+    
+    await ctx.send(response)
